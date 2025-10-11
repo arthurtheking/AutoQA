c2ba3396daa756c96bc34f84978b08c8635cd3df
Artur Kudria <akudria@Arturs-MacBook-Air.local>
2025-10-11 19:51:10 +0300

    added async/await hw

--- lesson 6/src/async_await.js
+++ lesson 6/src/async_await.js
@@ -7,3 +7,46 @@
 // і перевикористати цей JSON у іншій функції.
 
 // "https://jsonplaceholder.typicode.com/todos/1"
+
+async function getObjectByUrl(url) {
+    console.log('Starting operation...');
+
+    try {
+        console.log('Fetching from resource...')
+        
+        const response = await fetch(url);
+
+        if(response.ok) {
+
+            console.log('Verifying received data...')
+            const data = await response.json();
+
+            countUsers(data);
+        } else {
+            throw new Error('Received data is not applicable')
+        }
+
+    } catch(error) {
+
+        error.message = `Error while fetching object from ${url}`;
+        error.name = 'FetchError'
+
+        console.log(`${error.name}: ${error.message}`)
+    } finally {
+        console.log('Operation finished!')
+    }
+
+    
+}
+
+function countUsers(users) {
+
+    if(typeof(users) === "object" && !Array.isArray(users)) {
+        console.log('Counting Users...')
+        console.log("Number of users:", 1)
+    } else {
+        console.log(users.length)
+    }
+}
+
+getObjectByUrl('https://jsonplaceholder.typicode.com/todos/1')
